5f6c4e84d1159ffdca7a16cd5291cd347ac988af
harshalitalele <harshalitalele@gmail.com>
2019-03-01 17:18:33 +0530

    code for our slide images added

--- src/viewer/image/image-container.component.ts
+++ src/viewer/image/image-container.component.ts
@@ -9,11 +9,12 @@ import * as getOSDViewer from './webviewer-wrapper';
 })
 export class ImageContainerComponent implements OnInit {
   @Input() ids: string[];
+  myViewer;
 
   ngOnInit(): void {
-    getOSDViewer(OpenSeadragon.Viewer);
+    this.myViewer =  getOSDViewer(OpenSeadragon.Viewer);
     // initialize openseadragon
-    OpenSeadragon({
+    /*OpenSeadragon({
       id:            'openseadragon',
       tileSources:   [
         'https://openseadragon.github.io/example-images/highsmith/highsmith.dzi'
@@ -21,7 +22,21 @@ export class ImageContainerComponent implements OnInit {
       showNavigator: true,
       navigatorAutoFade: false,
       showNavigationControl: false
-    });
+    });*/
+
+    const schemaURIs = 'http://172.28.42.142:8090',
+      canvasId = 'openseadragon',
+      getOsdCallback = function(osdObj) {
+        /*self.osd = osdObj;
+        self.getOsdCB();*/
+        alert('get osd callback');
+      }, annotationHandlers = {},
+      imageChangeHandler = {},
+      showFullPageControl = false,
+      imageName = this.ids[1];
+    this.myViewer(schemaURIs, canvasId, getOsdCallback,
+      annotationHandlers, imageChangeHandler, showFullPageControl,
+      imageName, OpenSeadragon);
     // initialize our wrapper on top of it
     alert(this.ids);
   }
